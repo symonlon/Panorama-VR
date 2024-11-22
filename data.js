cad4d37d41552db74e38523cc90cfafe2b1ab030
symonlon <xml150@163.com>
2024-11-22 18:10:23 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,115 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "主卧室",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -2.0720324920439097,
+        "pitch": 0,
+        "fov": 1.4661211029866583
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.953842513021522,
+          "pitch": 0.6401872705943248,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.834375994489914,
+          "pitch": 0.3739348267425182,
+          "title": "卧室",
+          "text": "50平米"
+        },
+        {
+          "yaw": 1.116411475243428,
+          "pitch": 0.20773955060775862,
+          "title": "标题",
+          "text": "描述"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "次卧室",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5230983353069165,
+          "pitch": 1.101154065327071,
+          "rotation": 4.71238898038469,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.1180324642942292,
+          "pitch": 0.7657518643453542,
+          "title": "次卧室",
+          "text": "30平米"
+        }
+      ]
+    }
+  ],
+  "name": "全景图_202411221141",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
